3d3789d7ae8c811eaa788876ceb34c9f52b68c34
efrxngg <efrenpgc2602@gmail.com>
2025-08-07 21:04:15 -0500

    temp

--- src/main/java/com/bancodellitoral/core/infrastructure/controller/GetGreetingCustomerRestController.java
+++ src/main/java/com/bancodellitoral/core/infrastructure/controller/GetGreetingCustomerRestController.java
@@ -2,6 +2,7 @@ package com.bancodellitoral.core.infrastructure.controller;
 
 import com.bancodellitoral.core.application.GreetingCustomer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,14 @@ import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class GetGreetingCustomerRestController {
 
     private final GreetingCustomer greetingCustomer;
 
     @GetMapping("greets")
     public ResponseEntity<String> getGreetingCustomer() {
+        log.info("user");
         return ok(greetingCustomer.greeting());
     }
 
